e7c06a36da8c9634b097927089b1ef99fdb13a50
Franklinson <franklinhassey@gmail.com>
2024-05-09 02:12:06 +0000

    Change the model style

--- commercials/admin.py
+++ commercials/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
-from commercials.models import Label, Unit, Brand
+from commercials.models import Label, Unit, Brand, NutrientList, Nutrient
 
-@admin.register(Label)
+
+class NutrientInline(admin.StackedInline):
+    model = Nutrient
+    extra = 3
+
+
+
+# @admin.register(Label)
 class LabelAdmin(admin.ModelAdmin):
     list_display = ("categories", "name", "brand", "energy")
     list_filter = ("categories",)
-    search_fields = ("brand", "name")
-# admin.site.register(models.Label)
+    search_fields = ("brand__brand", "name")
+    fieldsets = [
+        (None, {"fields": ["categories", "brand", "name", "net_weight", "net_weight_unit",
+                           "serving_size", "serving_size_unit", "serving_quantity", "serving_quantity_unit",
+                           "per", "per_unit", "energy", "energy_unit"]}),
+        # ("Date information", {"fields": ["brand"], "classes": ["collapse"]}),
+    ]
+    inlines = [NutrientInline]
+
+
+admin.site.register(Label,LabelAdmin)
+# admin.site.register(Nutrient)
 
 
 @admin.register(Unit)
@@ -17,12 +34,14 @@ class UnitAdmin(admin.ModelAdmin):
     search_fields = ("name", )
     
     
-# @admin.register(Category)
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display = ("categories", )
-    
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ("brand", )
-    search_fields = ("name", )
+    search_fields = ("brand", )
+    
+
+@admin.register(NutrientList)
+class NutrientListAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
